442e218b620b1c5f4782bc0a3112dea6792e4ce6
Nate Traugh <n.traugh@yahoo.com>
2022-08-11 12:00:50 -0400

    added comments to the CartItem componenet

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -19,17 +19,21 @@ const CartItem = ({ id, quantity }: CartItemProps) => {
             <img src={item?.imgUrl} style={{ width: "125px", height: "75px", objectFit: "cover" }} />
             <div className="me-auto">
                 <div>
+                    {/* display the item name and a space, then IF the quantity is greater than 1 we will show the span class */}
                     {item?.name} {" "}
                     {quantity > 1 && (
                         <span className="text-muted" style={{fontSize: ".9rem"}}>
                             x{quantity}</span>
                     )}
                 </div>
+                {/* display our price of the single item */}
                 <div className="text-muted" style={{"fontSize": ".9rem"}}>
                         {FormatCurrency(item?.price)}
                 </div>
             </div>
+            {/* multiply the price by the quantity for a total  */}
             <div>{FormatCurrency(item?.price * quantity )}</div>
+            {/* on click we remove from the cart */}
             <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>
                 &times;
             </Button>
