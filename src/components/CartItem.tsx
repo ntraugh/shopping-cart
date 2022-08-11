2af6203968b628fe288a2ca04d659503d77380c0
Nate Traugh <n.traugh@yahoo.com>
2022-08-11 11:57:25 -0400

    added logic for adding total of each item quantity, added remove from cart function to cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "react-bootstrap"
+import { Button, Stack } from "react-bootstrap"
 import { useShoppingCart } from "../context/ShoppingCartContext"
 import storeItems from "../data/items.json"
 import FormatCurrency from "../utils/FormatCurrency"
@@ -18,8 +18,21 @@ const CartItem = ({ id, quantity }: CartItemProps) => {
         <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
             <img src={item?.imgUrl} style={{ width: "125px", height: "75px", objectFit: "cover" }} />
             <div className="me-auto">
-
+                <div>
+                    {item?.name} {" "}
+                    {quantity > 1 && (
+                        <span className="text-muted" style={{fontSize: ".9rem"}}>
+                            x{quantity}</span>
+                    )}
+                </div>
+                <div className="text-muted" style={{"fontSize": ".9rem"}}>
+                        {FormatCurrency(item?.price)}
+                </div>
             </div>
+            <div>{FormatCurrency(item?.price * quantity )}</div>
+            <Button variant="outline-danger" size="sm" onClick={() => removeFromCart(item.id)}>
+                &times;
+            </Button>
         </Stack>
     )
 }
